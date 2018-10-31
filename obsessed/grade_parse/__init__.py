8e295a9f02aa21e4bfc92f2db62138675f188d9f
Finaris <metorres@mit.edu>
2018-10-31 00:08:17 -0400

    Fixed import error in grade_parse/__init__.py to make it work with Python 3 as well as Python 2.

--- obsessed/grade_parse/__init__.py
+++ obsessed/grade_parse/__init__.py
@@ -1,5 +1,5 @@
 # Import relevant modules from the directory.
-from grade_sheet import GradeSheet
+from .grade_sheet import GradeSheet
 
 # Define which modules to export when importing grade_parse.
 __all__ = ['GradeSheet']
